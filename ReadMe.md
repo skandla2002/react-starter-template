cd22c1586075f0d2d461f1c493d504daec823cd3
skandla2002 <skandla2002@gmail.com>
2019-06-15 14:08:47 +0900

    initReactStater

---
+++ ReadMe.md
@@ -0,0 +1,14 @@
+리액트 라우터(React Router), 핫 모듈 리플레이스먼트(HMR, Hot Module Replacement), 경로(Route)와 벤더(Vendor)로 코드 분할 및 배포 설정 구성까지, 웹팩(Webpack)과 리액트(React) 사용법의 모든 것을 알아보자.
+
+아래는 앞으로 사용할 라이브러리 목록이다.
+
+React 16
+React Router 4
+Semantic UI : CSS 프레임워크
+Hot Module Replacement (HMR)
+CSS Autoprefixer
+CSS Modules
+Stage 1 Preset
+Webpack 4
+Route과 Vendor로 코드 분할
+Webpack Bundle Analyzer
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+
+module.exports = {
+    // webpack 설정부분
+    mode: 'development',
+    entry: './src/index.js', // 진입 지점(4에서 생략시 src 하위)
+    output: {
+        filename: 'bundle.[hash].js' // 번들된 파일 이름을 말한다.
+    }, // 컴파일된 파일을 저장할 경로를 알려준다.
+    devtool: 'inline-source-map', // 애플리케이션 디버깅을 도와준다, 개발시에만 inline-source-map 사용
+    module: { // 애플리케이션 내 포함되는 모듈을 정의, ESNext(바벨), CSS 모듈 
+        rules: [ // 각 모듈을 처리하는 방법을 설정한다.
+            // 두번째 룰 : node_modules 디렉터리를 제외한 자바스크립트 파일을 찾은 다음 babel-loader를 통해 바벨을 사용해 바닐라 자바스크립트로 변환한다. 바벨은 .babelrc 파일에서 설정 내용을 읽는다.
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+            // 첫번째 룰 : css파일을 찾고 style-loader와 css-loader로 css를 처리한다. 그 다음 css-loader에게 CSS모듈 카멜 케이스(camel case), 소스맵을 사용할 것을 지시
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        // options: {
+                        //     importLoaders: 2,
+                        //     modules: true,
+                        //     camelCase: true,
+                        //     // sourceMap: true,
+                        //     localIdentName: '[path][name]__[local]--[hash:base64:5]',
+                        // }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port, // 기본 3000
+        historyApiFallback: true,
+        open: true // 브라우저 자동 열림
+    }
+};
